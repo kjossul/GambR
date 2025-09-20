e6f36478bb9531c5615024f8e259e580abaea976
kjossul <riccalnov@gmail.com>
2025-09-20 11:16:34 +0200

    moved to fastapi

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from piccolo.engine import engine_finder
 from piccolo_admin.endpoints import create_admin
-from piccolo_api.crud.endpoints import PiccoloCRUD
-from starlette.applications import Starlette
-from starlette.routing import Mount, Route
-from starlette.staticfiles import StaticFiles
 
-from home.endpoints import HomeEndpoint, routes
+from home.endpoints import app as home_app
 from home.piccolo_app import APP_CONFIG
-from home.tables import Task
 
 
 async def open_database_connection_pool():
@@ -29,25 +26,17 @@ async def close_database_connection_pool():
 
 
 @asynccontextmanager
-async def lifespan(app: Starlette):
+async def lifespan(app: FastAPI):
     await open_database_connection_pool()
     yield
     await close_database_connection_pool()
 
 
-app = Starlette(
-    routes=[
-        Route("/", HomeEndpoint),
-        Mount(
-            "/admin/",
-            create_admin(
-                tables=APP_CONFIG.table_classes,
-                # Required when running under HTTPS:
-                # allowed_hosts=['my_site.com']
-            ),
-        ),
-        Mount("/static/", StaticFiles(directory="static")),
-        *routes
-    ],
-    lifespan=lifespan,
-)
+app = FastAPI()
+app.mount("/", home_app)
+app.mount("/admin", create_admin(
+    tables=APP_CONFIG.table_classes,
+    # Required when running under HTTPS:
+    # allowed_hosts=['my_site.com']    
+))
+app.mount("/static", StaticFiles(directory="static"), name="static")
--- home/endpoints.py
+++ home/endpoints.py
@@ -1,57 +1,45 @@
-import os
-
-import jinja2
-from starlette.endpoints import HTTPEndpoint
-from starlette.responses import HTMLResponse, JSONResponse
-from starlette.exceptions import HTTPException
-from starlette.routing import Route
-import requests
-import json
-import secrets
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
 from tables import *
+from schemas import *
+import requests
+import secrets
 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+# searchpath=os.path.join(os.path.dirname(__file__), "templates")
 
-ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(
-        searchpath=os.path.join(os.path.dirname(__file__), "templates")
+@app.get('/', response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("home.html.jinja", {
+        "request": request,
+        "title": "Piccolo + ASGI"
+    })
+
+@app.post('/auth')
+async def post(auth: Auth):
+    # send authentication from plugin to openplanet to verify client
+    op_url = "https://openplanet.dev/api/auth/validate"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "token": auth.token,
+        "secret": None
+    }  # todo (validate data before sending also to avoid unneccessary calls)
+    r = requests.post(op_url, data=data, headers=headers)
+    user = r.json()
+    if "error" in user:
+        raise HTTPException(status_code=400, detail="Invalid authentication")
+    # generate secret and update player table
+    token = secrets.token_hex(32)
+    q = Player.insert(
+        Player(uuid=user["account_id"], name=user["display_name"], secret=token)
+    ).on_conflict(
+        action="DO UPDATE",
+        values=[Player.name, Player.secret]
     )
-)
-
-
-class HomeEndpoint(HTTPEndpoint):
-    async def get(self, request):
-        template = ENVIRONMENT.get_template("home.html.jinja")
-
-        content = template.render(
-            title="Piccolo + ASGI",
-        )
-
-        return HTMLResponse(content)
-
-class AuthEndpoint(HTTPEndpoint):
-    async def post(self, request):
-        # send authentication from plugin to openplanet to verify client
-        op_url = "https://openplanet.dev/api/auth/validate"
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-        data = {
-            "token": request.json()["token"],
-            "secret": None
-        }  # todo (validate data before sending also)
-        r = requests.post(op_url, data=data, headers=headers)
-        user = r.json()
-        if "error" in user:
-            raise HTTPException(status_code=400, detail="Invalid authentication")
-        # generate secret and update player table
-        secret = secrets.token_hex(32)
-        q = Player.insert(
-            Player(uuid=user["account_id"], name=user["display_name"], secret=secret)
-        ).on_conflict(
-            action="DO UPDATE",
-            values=[Player.name, Player.secret]
-        )
-        q.run_sync()
-        return JSONResponse({"secret": secret})
-    
-routes = [Route('/auth', AuthEndpoint)]
+    q.run_sync()
+    return Auth(token=token)
